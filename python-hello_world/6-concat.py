65f2f983091cc24db4a5bcc88ea5438ac66dad86
ghalyahk <ghalyah1166@gmail.com>
2025-11-24 18:44:43 +0000

    Write a Python script that prints exactly

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Welcome to "  # define first part of the message
-str2 = "Holberton School!"  # define second part of the message
+str1 = "Welcome to "  # first part of the message
+str2 = "Holberton School!"  # second part of the message
 print(str1 + str2)
