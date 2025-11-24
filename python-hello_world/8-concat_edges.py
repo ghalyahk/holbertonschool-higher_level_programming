2922e8dde668080b7abfdd93f9ab5149cff8124c
ghalyahk <ghalyah1166@gmail.com>
2025-11-24 18:55:21 +0000

    Write a Python script that prints exactly

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-str1 = "object-"
-str2 = "oriented "
-str3 = "programming "
-str4 = "with "
-str5 = "Python"
+str1, str2, str3, str4, str5 = "object-", "oriented ", "programming ", "with ", "Python"
 print(str1 + str2 + str3 + str4 + str5)
