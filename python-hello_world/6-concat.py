135b2152263dd5603fd62157951db1b2c6681b0a
ghalyahk <ghalyah1166@gmail.com>
2025-11-25 13:57:04 +0000

    the zen of python

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Welcome to "  # first part of the message
-str2 = "Holberton School!"  # second part of the message
-print(str1 + str2)
+str1 = "Welcome to"
+str2 = "Holberton School!"
+print(str1 + " " + str2)
