b09f43f8aa7a4c3030cc2f761738635d769009e0
ghalyahk <ghalyah1166@gmail.com>
2025-12-01 16:05:33 +0000

    Print a list of integers... in reverse!

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     """Return a new list with element replaced at index idx."""
-    # إنشاء نسخة من القائمة الأصلية
     new_list = my_list.copy()
-    
-    # التحقق من صحة الفهرس
+
     if 0 <= idx < len(my_list):
         new_list[idx] = element
 
