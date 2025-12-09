ee500e653c34b9b03f196098fa71f810e5bf0537
ghalyahk <ghalyah1166@gmail.com>
2025-12-09 23:08:25 +0000

    Simple rectangle

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -63,9 +63,10 @@ class Rectangle:
         """Return string representation using '#'."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(
-            "#" * self.__width for _ in range(self.__height)
-        )
+        lines = []
+        for _ in range(self.__height):
+            lines.append("#" * self.__width)
+        return "\n".join(lines)
 
     def __repr__(self):
         """Return recreatable string representation."""
