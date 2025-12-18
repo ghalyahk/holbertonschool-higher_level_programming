4107583a1e0099ae0f39848ad7708a99f3e0684b
ghalyahk <ghalyah1166@gmail.com>
2025-12-18 22:42:00 +0000

    Shapes, Interfaces, and Duck Typing

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,41 +1,21 @@
 #!/usr/bin/env python3
-from abc import ABC, abstractmethod
-import math
 
 
-class Shape(ABC):
-    @abstractmethod
-    def area(self):
-        pass
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
 
-    @abstractmethod
-    def perimeter(self):
-        pass
+    def extend(self, iterable):
+        count = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{count}] items.")
 
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
 
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-
-    def area(self):
-        return math.pi * (self.radius ** 2)
-
-    def perimeter(self):
-        return 2 * math.pi * self.radius
-
-
-class Rectangle(Shape):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        return 2 * (self.width + self.height)
-
-
-def shape_info(shape):
-    print(f"Area: {shape.area()}")
-    print(f"Perimeter: {shape.perimeter()}")
+    def pop(self, index=-1):
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
