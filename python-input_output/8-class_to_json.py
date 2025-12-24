426ac62707e15903f8cff7e1957194444a7db720
ghalyahk <ghalyah1166@gmail.com>
2025-12-24 06:08:40 +0000

    Write a function that writes an Object to a text file, using a JSON representation

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Function that returns the dictionary description of a class for JSON serialization"""
+"""Function that returns the dictionary description of a class 
+for JSON serialization
+"""
 
 
 def class_to_json(obj):
     """
     Returns the dictionary representation of obj's attributes
+
     Args:
         obj: instance of a class
+
     Returns:
         dict containing all attributes of obj
     """
