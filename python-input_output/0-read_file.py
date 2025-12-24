ffe9466a92ccb34e707db2590d89339b2cd152ee
ghalyahk <ghalyah1166@gmail.com>
2025-12-24 04:15:06 +0000

    You are not allowed to import any module

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for reading a text file and printing it to stdout"""
 
+
 def read_file(filename=""):
     """Reads a text file (UTF-8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
