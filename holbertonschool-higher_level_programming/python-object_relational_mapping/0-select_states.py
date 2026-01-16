f037b318c00b3fbde4663104f0d68584037b7420
ghalyahk <ghalyah1166@gmail.com>
2026-01-16 02:06:31 +0000

    Import the database dump from hbtn_0d_tvshows

--- holbertonschool-higher_level_programming/python-object_relational_mapping/0-select_states.py
+++ holbertonschool-higher_level_programming/python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa."""
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
 
-import sys
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    user = sys.argv[1]
+    username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
-        user=user,
+        user=username,
         passwd=password,
-        db=db_name
+        db=db_name,
+        port=3306
     )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
 
-    for row in cur.fetchall():
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for row in cursor.fetchall():
         print(row)
 
-    cur.close()
+    cursor.close()
     db.close()
